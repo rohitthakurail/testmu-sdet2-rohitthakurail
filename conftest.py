bac74f77f84ce9781d21df5a099c9027d5d652be
rohit thakurail <rohitthakurail@gmail.com>
2026-05-25 01:11:46 +0530

    added chrome headless and other arguments

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 @pytest.fixture
 def driver(request):
@@ -10,10 +10,17 @@ def driver(request):
     browser = getattr(request, "param", "chrome")
 
     if browser == "firefox":
+        firefox_options = FirefoxOptions()
+        firefox_options.add_argument("--headless")
         driver = webdriver.Firefox()
     else:
         options = Options()
         options.add_argument("--guest")
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+
         driver = webdriver.Chrome(options=options)
 
     yield driver
